352108708d0334e1f59bc81bf910a099de07ff1d
UsmanAhsan098 <usmanahsan098@gmail.com>
2021-03-13 22:42:39 +0300

    init

---
+++ subjects.js
@@ -0,0 +1,82 @@
+let arr = [
+    {
+                    "id": 1001,
+                    "title": "<b>Maths</b>",
+                    "location": "London",
+                    "price": 6,
+                    "image": "math logo.png",
+                    "availableInventory": 5
+    },
+    {
+                    "id": 1002,
+                    "title": "<b>English</b>",
+                    "location": "West Ham",
+                    "price": 2,
+                    "image": "english logo.png",
+                    "availableInventory": 5
+    },
+    {
+                    "id": 1003,
+                    "title": "<b>Physics</b>",
+                    "location": "Tottenham",
+                    "price": 90,
+                    "image": "physics logo.png",
+                    "availableInventory": 5
+    },
+    {
+                    "id": 1004,
+                    "title": "<b>Chemistry</b>",
+                    "location": "Bristol",
+                    "price": 80,
+                    "image": "chemistry logo.png",
+                    "availableInventory": 5
+    },
+    {
+                    "id": 1005,
+                    "title": "<b>History</b>",
+                    "location": "Liverpool",
+                    "price": 70,
+                    "image": "history logo.png",
+                    "availableInventory": 5
+    },
+    {
+                    "id": 1006,
+                    "title": "<b>Arabic</b>",
+                    "location": "Manchester",
+                    "price": 60,
+                    "image": "arabic logo.png",
+                    "availableInventory": 5
+    },
+    {
+                    "id": 1007,
+                    "title": "<b>Maths</b>",
+                    "location": "birmingham",
+                    "price": 55,
+                    "image": "math logo.png",
+                    "availableInventory": 5
+    },
+    {
+                    "id": 1008,
+                    "title": "<b>P.E</b>",
+                    "location": "Bradford",
+                    "price": 50,
+                    "image": "pe logo.png",
+                    "availableInventory": 5
+    },
+    {
+                    "id": 1009,
+                    "title": "<b>Law</b>",
+                    "location": "Cambrge",
+                    "price": 45,
+                    "image": "law logo.png",
+                    "availableInventory": 5
+    },
+    {
+                    "id": 1010,
+                    "title": "<b>Robotics</b>",
+                    "location": "Oxford",
+                    "price": 40,
+                    "image": "robotics logo.png",
+                    "availableInventory": 5
+    }
+]
